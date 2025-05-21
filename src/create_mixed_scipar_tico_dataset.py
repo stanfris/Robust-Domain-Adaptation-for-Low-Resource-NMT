1896813c109f80403f20ab7f1b83249f2a75c2af
Stan <stanfris2.0@gmail.com>
2025-05-21 22:37:13 +0200

    fixed data creation

--- src/create_mixed_scipar_tico_dataset.py
+++ src/create_mixed_scipar_tico_dataset.py
@@ -31,8 +31,8 @@ from datasets import load_from_disk, load_dataset, Dataset, DatasetDict
 from modules import *
 
 # Load the combined dataset from disk
-train_dataset = load_from_disk('data/mixed_dataset')
-dev_dataset = load_dataset('sethjsa/medline_ru_parallel')
+train_dataset = load_from_disk('data/mixed_dataset_tico_scipar')
+dev_dataset = load_dataset('sethjsa/tico_en_ru')
 
 # Select 20 start examples (reproducibly)
 # random_examples_1 = train_dataset['train'].select(range(20))
@@ -44,14 +44,14 @@ model, tokenized_dev_dataset, tokenizer, training_args = 1, 1, 1, 1
 dev_sample_percentage = 0.05
 save_percentage = 0.05
 train_split="train"
-dev_split="train"
+dev_split="test"
 # print length of train_dataset
 print(f"Length of train_dataset: {len(train_dataset['train'])}")
 print("Loading model...")	
 selected_data, indices = select_data_subset(model, train_dataset, dev_dataset, tokenized_dev_dataset, dev_sample_percentage, save_percentage, tokenizer, training_args, train_split=train_split, dev_split=dev_split, selection_method="bm25", src_lang="en", output_lang="ru", return_indices=True)
 
 # print what percentage of indices are below 2100
-print(f"Percentage of indices below 2100 bm25: {sum([1 for i in indices if i > 90000]) / len(indices) * 100:.2f}%")
+print(f"Percentage of indices below 2100: {sum([1 for i in indices if i <2100]) / len(indices) * 100:.2f}%")
 # # print the indexed first 10 indices
 # indexed_train_dataset = train_dataset['train'].select(indices[:5])['en']
 # print("Indexed train dataset:")
@@ -62,10 +62,10 @@ print(f"Percentage of indices below 2100 bm25: {sum([1 for i in indices if i > 9
 # for i, ex in enumerate(selected_data['train']['en'][:5]):
 #     print(f"Example {i + 1}:")
 #     print(ex)
-# selected_data, indices = select_data_subset(model, train_dataset, dev_dataset, tokenized_dev_dataset, dev_sample_percentage, save_percentage, tokenizer, training_args, train_split=train_split, dev_split=dev_split, selection_method="5gram", src_lang="en", output_lang="ru", return_indices=True)
-# print(f"Percentage of indices below 2100 5gram: {sum([1 for i in indices if i > 10000]) / len(indices) * 100:.2f}%")
+selected_data, indices = select_data_subset(model, train_dataset, dev_dataset, tokenized_dev_dataset, dev_sample_percentage, save_percentage, tokenizer, training_args, train_split=train_split, dev_split=dev_split, selection_method="5gram", src_lang="en", output_lang="ru", return_indices=True)
+print(f"Percentage of indices below 2100: {sum([1 for i in indices if i <2100]) / len(indices) * 100:.2f}%")
 
 
 selected_data, indices = select_data_subset(model, train_dataset, dev_dataset, tokenized_dev_dataset, dev_sample_percentage, save_percentage, tokenizer, training_args, train_split=train_split, dev_split=dev_split, selection_method="random", src_lang="en", output_lang="ru", return_indices=True)
 
-print(f"Percentage of indices below 2100 bm25: {sum([1 for i in indices if i > 90000]) / len(indices) * 100:.2f}%")
+print(f"Percentage of indices below 2100: {sum([1 for i in indices if i <2100]) / len(indices) * 100:.2f}%")
